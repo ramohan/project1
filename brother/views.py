7c7168809f9a92b13201ea1371c0d608c3938617
ramohan <ramamohan20@yahoo.com>
2015-02-05 10:50:29 +0530

    written new function in views and removed existing html code

--- brother/views.py
+++ brother/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
-<html>
-<head>
-<title> login page</title>
-</head>
-<body>
-<form action="check_login" method="GET">
-user name<input type='text' name='user'>
-password<input type='password' name='pwd'>
-<input type='submit' value='login'>
-</form>
-</body>
-</html>
+
 # Create your views here.
+
+
+# Another changes....
+
+
+def new_fuc(request):
+    pass
